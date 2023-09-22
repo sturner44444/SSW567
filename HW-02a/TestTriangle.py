e4c7d364665c549d58824112322fad49ff8553c3
sturner44444 <sturner4@stevens.edu>
2023-09-21 22:55:18 -0400

    add triangle code

--- HW-02a/TestTriangle.py
+++ HW-02a/TestTriangle.py
@@ -18,13 +18,42 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Right')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(13,12,5),'Right')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
+
+    def testScaleneTriangleA(self):
+        self.assertEqual(classifyTriangle(1,3,4),'Scalene')
+
+    def testScaleneTriangleB(self):
+        self.assertEqual(classifyTriangle(9,6,3),'Scalene')
+
+    def testIsoscelesTriangleA(self):
+        self.assertEqual(classifyTriangle(1,1,2),'Isosceles')
+
+    def testIsoscelesTriangleB(self):
+        self.assertEqual(classifyTriangle(3,2,2),'Isosceles')
+
+    def testValidTriangleA(self):
+        self.assertEqual(classifyTriangle(1,2,10),'NotATriangle')
+
+    def testValidTriangleB(self):
+        self.assertEqual(classifyTriangle(200,200,200),'Equilateral')
+
+    def testValidTriangleC(self):
+        self.assertEqual(classifyTriangle(0,0,0),'InvalidInput')
+
+    def testValidTriangleD(self):
+        self.assertEqual(classifyTriangle(0,-1,1),'InvalidInput')
+
+    def testValidTriangleE(self):
+        self.assertEqual(classifyTriangle(1.5,2.5,3.5),'InvalidInput')
+        
+        
 
 if __name__ == '__main__':
     print('Running unit tests')
