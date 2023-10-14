29353fe5390592988e6a8f3052aafec58998dd0f
sturner44444 <144182901+sturner44444@users.noreply.github.com>
2023-10-14 19:06:17 -0400

    Update hw4atest.py

--- GitHubApi567-hw4a/hw4atest.py
+++ GitHubApi567-hw4a/hw4atest.py
@@ -5,12 +5,14 @@ import hw4a
 
 class TestHW4a(unittest.TestCase):
 
+    @patch('builtins.input', return_value='sturner44444')
     @patch('hw4a.validID', return_value=True)
-    def test_user(self, mock_validID):
+    def test_user(self, mock_validID, mock_input):
         self.assertEqual(hw4a.validID('sturner44444'), True)
 
+    @patch('builtins.input', return_value='sturner44444')
     @patch('hw4a.getrepos', return_value=['SSW567', 1])
-    def test_repos(self, mock_getrepos):
+    def test_repos(self, mock_getrepos, mock_input):
         self.assertIn('SSW567', hw4a.getrepos('sturner44444'))
         self.assertNotIn('classifysquare', hw4a.getrepos('sturner44444'))
         self.assertIn(1, hw4a.getrepos('sturner44444'))
