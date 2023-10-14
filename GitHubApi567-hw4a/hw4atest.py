3218f143c93a04c8a9889761fc1f1cc3172ce3cf
sturner44444 <144182901+sturner44444@users.noreply.github.com>
2023-10-14 18:35:10 -0400

    Update hw4atest.py

--- GitHubApi567-hw4a/hw4atest.py
+++ GitHubApi567-hw4a/hw4atest.py
@@ -1,16 +1,20 @@
 import unittest
+from unittest.mock import patch, Mock
+
 import hw4a
 
 class TestHW4a(unittest.TestCase):
 
-    def test_user(self):
+    @patch('hw4a.validID', return_value=True)
+    def test_user(self, mock_validID):
         self.assertEqual(hw4a.validID('sturner44444'), True)
 
-    def test_repos(self):
+    @patch('hw4a.getrepos', return_value=['SSW567', 1])
+    def test_repos(self, mock_getrepos):
         self.assertIn('SSW567', hw4a.getrepos('sturner44444'))
         self.assertNotIn('classifysquare', hw4a.getrepos('sturner44444'))
-        self.assertIn(13, hw4a.getrepos('sturner44444'))
-        self.assertNotIn(15, hw4a.getrepos('sturner44444'))      
+        self.assertIn(1, hw4a.getrepos('sturner44444'))
+        self.assertNotIn(100, hw4a.getrepos('sturner44444'))      
 
 if __name__=='__main__':
     unittest.main()
